f6fd08d6658c6c82d1824ee7e846cb3c25054197
Lupalufa <lupalufa431@gmail.com>
2025-04-14 17:29:34 -0300

    feat: Adiciona estrutura inicial do frontend com React e Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Home from './pages/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./Home.module.css"
+import Header from "../../components/Comuns/Header/Header";
+import Main from "../../components/Comuns/Main/Main";
+import Footer from "../../components/Comuns/Footer/Footer";
+
+function Home(){
+    return(
+        <>
+        <Header />
+        <Main />
+        <Footer />
+        </>
+    )
+}
+
+export default Home
